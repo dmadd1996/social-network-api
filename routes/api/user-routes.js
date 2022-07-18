5ba577641d106b519c24e48f6af0be0ed8bfc5a9
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-07-17 20:27:05 -0500

    added friend adding and deletion.

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,9 +5,13 @@ const {
     createUser,
     updateUser,
     deleteUser,
+    addFriend,
+    removeFriend,
 } = require('../../controllers/user-controller')
 
 router.route('/').get(getUsers).post(createUser)
 router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser)
+router.route('/:userId/add/:friendId').put(addFriend)
+router.route('/:userId/remove/:friendId').put(removeFriend)
 
 module.exports = router
