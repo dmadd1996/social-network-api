112d3ce715f39a7cffecd85fece05fad80aed456
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-07-17 20:33:19 -0500

    added friend adding and deleting

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -55,6 +55,34 @@ const userController = {
                 res.json(dbUserData);
             })
             .catch(err => res.status(400).json(err));
+    },
+    addFriend(req, res) {
+        User.updateOne(
+            { _id: req.params.userId },
+            { $push: {friends: { _id: req.params.friendId}}}
+            )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    removeFriend(req, res) {
+        User.updateOne(
+            { _id: req.params.userId },
+            { $pull: {friends: { _id: req.params.friendId}}}
+            )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
     }
 }
 
