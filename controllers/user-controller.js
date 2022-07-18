6f1c61c67e63dbb99172aae6c293136ac24badc5
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-07-17 19:22:17 -0500

    fixed post and delete routes for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -35,7 +35,7 @@ const userController = {
             })
     },
     updateUser({ params, body }, res) {
-        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        User.findOneAndUpdate({ _id: params.userId }, body, { new: true })
             .then(dbUserData => {
                 if (!dbUserData) {
                     res.status(404).json({ message: 'No user found with this id!' });
@@ -46,7 +46,7 @@ const userController = {
             .catch(err => res.status(400).json(err));
     },
     deleteUser({ params }, res) {
-        User.findOneAndDelete({ _id: params.id })
+        User.findOneAndDelete({ _id: params.userId })
             .then(dbUserData => {
                 if (!dbUserData) {
                     res.status(404).json({ message: 'No user found with this id!' });
